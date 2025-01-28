89ba43786fedbaff469e39838f43829d15d73595
Alias314 <83777973+Alias314@users.noreply.github.com>
2025-01-29 06:54:59 +0800

    Stored board in state, added drag and drop function

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,51 @@
-import { files, ranks } from "./chessData";
+import { files, ranks, initialBoard } from "./chessData";
+import { useEffect, useState } from "react";
 import Square from "./square";
 
 export default function Board() {
-    const board = Array(64).fill().map((_, i) => {
-        const file = i % 8;
-        const rank = 7 - Math.floor(i / 8);
-        const isDarkSquare = (file + rank) % 2 === 0;
+    const [board, setBoard] = useState(initialBoard);
 
-        return (
-            <Square 
-                key={i}
-                file={files[file]}
-                rank={ranks[rank]}
-                isDarkSquare={isDarkSquare}
-            />
-        );
-    });
+    const movePiece = (from, to) => {
+        console.log(from, to);
+        setBoard((prevBoard) => {
+            const nextBoard = { ...prevBoard };
+            nextBoard[to] = nextBoard[from];
+            delete nextBoard[from];
+            return nextBoard;
+        });
+    }
+
+    const resetBoard = () => {
+        setBoard(initialBoard);
+    }
 
     return (
-        <div className="grid grid-cols-8">
-            {board}
+        <div>
+            <div className="grid grid-cols-8">
+                {ranks.map((rank, i) =>
+                    files.map((file, j) => {
+                        const coordinate = file + rank;
+                        const isDarkSquare = (i + j) % 2 === 1;
+                        const piece = board[coordinate] || null;
+
+                        return (
+                            <Square 
+                                key={coordinate}
+                                coordinate={coordinate}
+                                isDarkSquare={isDarkSquare}
+                                piece={piece}
+                                movePiece={movePiece}
+                            />
+                        )
+                    })
+                )}
+            </div>
+            <button
+                onClick={resetBoard}
+                className="mt-2 p-2 text-white font-semibold bg-red-500 rounded-xl cursor-pointer"
+            >
+                Reset Board
+            </button>
         </div>
     );
 }
