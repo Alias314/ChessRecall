0235c38166a9b0b3f125b277fffdf6510d1d1344
Alias314 <83777973+Alias314@users.noreply.github.com>
2025-01-29 06:58:03 +0800

    Wrapped in react-dnd provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend"
 import Board from "./components/board";
 
 export default function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <Board />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="h-screen w-screen flex items-center justify-center">
+        <Board />
+      </div>
+    </DndProvider>
   );
 }
