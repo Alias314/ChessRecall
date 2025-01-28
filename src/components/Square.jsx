aa4e1f10cc4919e6f43eaebe468afe6aca4e60a3
Alias314 <83777973+Alias314@users.noreply.github.com>
2025-01-29 06:57:11 +0800

    Seperated image into Piece.jsx

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,37 +1,30 @@
-import { piecePosition } from "./chessData";
+import { useDrop } from "react-dnd";
+import Piece from "./Piece";
 
-export default function Square({ file, rank, isDarkSquare }) {
-    const coordinate = file + rank;
+export default function Square({ coordinate, isDarkSquare, piece, movePiece }) {
+    const [{ canDrop }, drop] = useDrop(() => ({
+        accept: 'Piece',
+        drop: (item) => movePiece(item.from, coordinate),
+    }));
     
     return (
         <div
-            draggable={true}
+            ref={drop}
             className={`h-18 w-18 flex items-center justify-center text-lg font-bold relative
                 ${isDarkSquare ? "bg-[#739552] text-[#EBECD0]" : "bg-[#EBECD0] text-[#739552]"}`}
         >
-            <div className="absolute bottom-0 right-1">
-                {rank === 1 && coordinate[0]}
-            </div>
             <div className="absolute top-0 left-1">
-                {file === "a" && coordinate[1]}        
+                {coordinate[0] === "a" && coordinate[1]}
             </div>
-
-            {piecePosition[coordinate] &&
-                <img 
-                    src={`assets/pieces/${piecePosition[coordinate]}.png`}
-                    className="h-15 w-15"
-                />
-            }
-            {rank === 2 &&
-                <img 
-                    src="assets/pieces/pawn_w.png"
-                    className="h-15 w-15"
-                />
-            }
-            {rank === 7 &&
-                <img 
-                    src="assets/pieces/pawn_b.png"
-                    className="h-15 w-15"
+            <div className="absolute bottom-0 right-1">
+                {coordinate[1] === '1' && coordinate[0]}
+            </div>
+            
+            {
+                piece &&
+                <Piece 
+                    coordinate={coordinate} 
+                    piece={piece} 
                 />
             }
         </div>
